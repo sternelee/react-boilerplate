54112a1a069ead844c3963c70edce72b689acc4b
lixingguang <lixingguang@xunlei.com>
2017-12-22 18:51:40 +0800

    react-fastify

--- react-fastify/.next/dist/pages/_document.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = require('next\\dist\\server\\document.js');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlc1xcbmV4dFxcZGlzdFxccGFnZXNcXF9kb2N1bWVudC5qcyJdLCJuYW1lcyI6WyJtb2R1bGUiLCJleHBvcnRzIiwicmVxdWlyZSJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoiX2RvY3VtZW50LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkQ6L1dXVy94dW5sZWkvcmVhY3QtYXBwcy9yZWFjdC1ib2lsZXJwbGF0ZS9yZWFjdC1mYXN0aWZ5In0=
