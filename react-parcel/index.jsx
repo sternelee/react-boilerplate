99a8e8aa1656665724d07be819805522f8013ccb
lixingguang <lixingguang@xunlei.com>
2017-12-25 15:55:50 +0800

    react-parcel

---
+++ react-parcel/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.styl";
+
+import TimerView from './components/TimerView.js';
+import appState from './appState.js';
+
+import DevTools from "mobx-react-devtools";
+
+import TodoList from "./components/TodoList";
+import TodoListModel from "./models/TodoListModel";
+
+const store = new TodoListModel();
+const App = () => (
+  <div className="App">
+    <h2>Hello Parcel</h2>
+    <DevTools />
+    <TodoList store={store} />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
