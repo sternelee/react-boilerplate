23e7fed090d372d0553ee297a408222102f33bb3
sternelee <sternelee@gmail.com>
2018-09-12 19:50:23 +0800

    去掉ts

--- react-ts-app/src/components/FooterItem.tsx
+++
@@ -1,5 +0,0 @@
-// import * as React from 'react';
-
-// const FooterItem = (props) => <span className={props.isOn ? 'on' : ''}>{props.nmae}</span>;
-
-// export default FooterItem;
--- react-ts-app/src/containers/Home.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const Home = () => (
-    <div>
-        Hello Home
-    </div>
-  );
